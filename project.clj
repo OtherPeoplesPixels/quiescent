2a4974ef1119a519fbba5285c59840bdfcbc7d9f
Kevin Neaton <neatonk@gmail.com>
2014-05-09 17:32:39 -0400

    Update react-cljs dep

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.facebook/react "0.9.0"]]
+                 [com.facebook/react "0.9.0.1"]]
   :source-paths ["src"]
 
   ;; development concerns
